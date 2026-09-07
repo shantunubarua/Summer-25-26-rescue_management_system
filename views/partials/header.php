bb2b058bed8d6e12265548448a6a963b1ad3b614
Shantunu007 <rohitbarua252003@gmail.com>
2026-09-07 23:03:20 +0600

    updating header "23-53712-3"

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rescue Management System</title>
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>
+        Rescue Management System
+    </title>
 
     <link
-    rel="stylesheet"
-    href="/rescue_management_system/assets/css/style.css?v=12"
->
+        rel="stylesheet"
+        href="assets/css/style.css?v=13"
+    >
+
 </head>
+
 <body>
