54b6d621c9b2bfde1f17496ad22cd7b61ac33838
Tanaka8647 <tanakarahman8@gmail.com>
2026-09-09 19:38:04 +0600

    updating helpers/auth "23-55678-3"

--- helpers/auth.php
+++ helpers/auth.php
@@ -73,3 +73,101 @@ function requireVolunteer()
         die("Access denied.");
     }
 }
+
+/*
+|--------------------------------------------------------------------------
+| CSRF TOKEN
+|--------------------------------------------------------------------------
+*/
+
+function getCsrfToken()
+{
+    if (
+        empty($_SESSION['csrf_token']) ||
+        !is_string($_SESSION['csrf_token'])
+    ) {
+
+        $_SESSION['csrf_token'] =
+            bin2hex(
+                random_bytes(32)
+            );
+    }
+
+    return $_SESSION['csrf_token'];
+}
+
+
+/*
+|--------------------------------------------------------------------------
+| CSRF HIDDEN INPUT
+|--------------------------------------------------------------------------
+*/
+
+function csrfField()
+{
+    $token =
+        getCsrfToken();
+
+    return
+        '<input type="hidden" name="csrf_token" value="' .
+        htmlspecialchars(
+            $token,
+            ENT_QUOTES,
+            'UTF-8'
+        ) .
+        '">';
+}
+
+
+/*
+|--------------------------------------------------------------------------
+| VALIDATE CSRF TOKEN
+|--------------------------------------------------------------------------
+*/
+
+function validateCsrfToken()
+{
+    $sessionToken =
+        $_SESSION['csrf_token']
+        ?? '';
+
+    $submittedToken =
+        $_POST['csrf_token']
+        ?? '';
+
+
+    if (
+        !is_string($sessionToken) ||
+        !is_string($submittedToken) ||
+        $sessionToken === '' ||
+        $submittedToken === ''
+    ) {
+
+        return false;
+    }
+
+
+    return hash_equals(
+        $sessionToken,
+        $submittedToken
+    );
+}
+
+
+/*
+|--------------------------------------------------------------------------
+| REQUIRE VALID CSRF TOKEN
+|--------------------------------------------------------------------------
+*/
+
+function requireValidCsrfToken()
+{
+    if (!validateCsrfToken()) {
+
+        http_response_code(403);
+
+        die(
+            "Invalid or expired form request."
+        );
+    }
+}
